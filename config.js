dee9f019a6acbbbaedbc35c3570c675618be9f71
Tanjiro2130K <127303425+Tanjiro2130K@users.noreply.github.com>
2024-12-12 08:04:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,22 +6,22 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "nAhjjKDa#TSY0sOG_JFXBdp2cz6tX4Ab-SH0XXRFMqdkpS9Bt4HM",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/nqCsIHZ.jpeg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M SANA_MD WHATSAPP BOT 😊♻️",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://fighter-programmer-uploaderf.hf.space/file/image-27aaeln9gh9.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "Ohayo Daring i am still Alive 🧣",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
-    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
     AUTO_REACT: process.env.AUTO_REACT || "true",
-    HEART_REACT: process.env.HEART_REACT || "true",
-    OWNER_REACT: process.env.OWNER_REACT || "true",
-    BOT_NAME: process.env.BOT_NAME || "➺ꜱᴀɴᴀ ᴋʜᴀɴ࿐",
+    HEART_REACT: process.env.HEART_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "false",
+    BOT_NAME: process.env.BOT_NAME || "Chitoge",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
